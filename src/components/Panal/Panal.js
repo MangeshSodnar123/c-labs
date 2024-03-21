7ffa1774b3126e00ba5fab8143959ea2f841b2e0
Mangesh Sodnar <mangeshsodnar123@gmail.com>
2024-03-21 13:03:45 +0530

    handleSaveSegment function looking good, webhook url still pending

--- src/components/Panal/Panal.js
+++ src/components/Panal/Panal.js
@@ -6,11 +6,45 @@ import Trait from "../Trait/Trait";
 import ReactDom from "react-dom";
 import { useState } from "react";
 
-export default function Panal({ open, children, onClose }) {
-
+export default function Panal({ open, onClose }) {
+  const [segmentName, setSegmentName] = useState("");
   const [schemas, setSchemas] = useState([]);
 
   if (!open) return null;
+
+  //saving the segment
+
+  const handleSaveSegment = async () => {
+    const data = {
+      segment_name: segmentName,
+      schema: schemas.map((schema) => ({
+        [schema.value]:
+          schema.label.charAt(0).toUpperCase() +
+          schema.label.slice(1).replace("_", " "),
+      })),
+    };
+    console.log("data: ",data)
+    try {
+      const response = await fetch("your-webhook-url", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log("Segment saved successfully!");
+        onClose(); // Close the panel after saving
+      } else {
+        console.error("Failed to save segment:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error saving segment:", error.message);
+    }
+  };
+
+  //saving the segment
 
   return ReactDom.createPortal(
     <>
@@ -21,8 +55,10 @@ export default function Panal({ open, children, onClose }) {
           <Typography>Enter the name of the Segment</Typography>
           <input
             type="text"
-            className="input-box" 
+            className="input-box"
             placeholder="Name of the segment"
+            value={segmentName}
+            onChange={(e) => setSegmentName(e.target.value)}
           />
 
           <Typography sx={{ marginTop: "20px" }}>
@@ -51,12 +87,14 @@ export default function Panal({ open, children, onClose }) {
             </Box>
           </Box>
 
-          <Trait schemas={schemas} setSchemas={setSchemas}>Add schema to segment</Trait>
+          <Trait schemas={schemas} setSchemas={setSchemas}>
+            Add schema to segment
+          </Trait>
         </div>
         <div className="panal-footer">
           <Button
             variant="contained"
-            onClick={onClose}
+            onClick={handleSaveSegment}
             sx={{
               backgroundColor: "#41B494",
               color: "white",
