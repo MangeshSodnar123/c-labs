ceee6ce4691bf291d4839c9b41195a65f8e6167f
Mangesh Sodnar <mangeshsodnar123@gmail.com>
2024-03-21 12:21:06 +0530

    UI Development done

--- src/components/SchemaDisplay/SchemaDisplay.js
+++ src/components/SchemaDisplay/SchemaDisplay.js
@@ -1,57 +1,42 @@
-// // SchemaDisplay.js
-// import React from "react";
-// import ColoredDot from "../ColoredDot/ColoredDot";
-// import SchemaDropdown from "../SchemaDropdown/SchemaDropdown";
-
-// export default function SchemaDisplay({ schemas }) {
-//   return (
-//     <div>
-//       {schemas.map((schema, index) => (
-//         <div key={index}>
-//           {schema.category === "user" ? (
-//             <ColoredDot color="green" size={16} />
-//           ) : (
-//             <ColoredDot color="red" size={16} />
-//           )}
-//           <span>{schema.label}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-
-///////////////////////////////////////////////////////////////////
-
-// SchemaDisplay.js
 import React from "react";
 import ColoredDot from "../ColoredDot/ColoredDot";
-import "./SchemaDisplay.css"
+import "./SchemaDisplay.css";
+import IconButton from "@mui/material/IconButton";
+import RemoveIcon from '@mui/icons-material/Remove';
 
 export default function SchemaDisplay({ schemas, allSchemas, onSchemaChange }) {
   return (
-    <div >
+    <div>
       {schemas.map((schema, index) => (
-        <div key={index}>
+        <div key={index} className="flexbox">
           {schema.category === "user" ? (
-            <ColoredDot color="green" size={16} />
+            <ColoredDot color="#5DDB78" size={16} />
           ) : (
-            <ColoredDot color="red" size={16} />
+            <ColoredDot color="#D24572" size={16} />
           )}
           <select
             value={schema.value}
             onChange={(event) => onSchemaChange(event, index)}
-            className="custom-dropdown"
+            className="custom-dropdown-d"
           >
-            {allSchemas.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
+            {allSchemas.map((schema) => (
+              <option
+                key={schema.value}
+                value={schema.value}
+                disabled={schemas.find(
+                  (selectedSchema) => selectedSchema.value === schema.value
+                )}
+                category={schema.category}
+              >
+                {schema.label}
               </option>
             ))}
           </select>
+          <IconButton aria-label="delete" size="small">
+            <RemoveIcon />
+          </IconButton>
         </div>
       ))}
     </div>
   );
 }
-
